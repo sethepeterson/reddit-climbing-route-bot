8fd6310b6b7d09eb85a6bdf537d0b8f18e08e4bf
Seth <sethpeterson@ku.edu>
2019-05-17 16:32:30 -0500

    Change API credentials to external file.

--- ClimbingRouteBot/SubmissionHandler.py
+++ ClimbingRouteBot/SubmissionHandler.py
@@ -23,90 +23,84 @@ class SubmissionHandler:
         #Represents the index of the titleWord that contains a rating.
         ratingIndex = 0
 
-        #Split submission title into list of submissionTitleWords.
-        submissionTitleWords = self.submission.title.split()
-        for titleWord in submissionTitleWords:
-            titleWordIndex = titleWordIndex + 1
 
-            #If current titleWord contains any rating  -->  note current index in ratingIndex.
-            ratings = ['4','5','6','7','8','9','10','11','12','13','14','15']
-            ratings.extend(['VB','V1','V2','V3','V4','V5','V6','V7','V8','V9','V10'])
-            if any (rating in titleWord for rating in ratings):
-                ratingIndex = titleWordIndex
+        ratings = ['4','5','6','7','8','9','10','11','12','13','14','15']
+        ratings.extend(['VB','V1','V2','V3','V4','V5','V6','V7','V8','V9','V10'])
+        if any (rating in self.submission.title for rating in ratings):
 
-                #The following lines of code determines the probable name of the route:
-                routeName = ''
-                titleWordIndex = titleWordIndex - 1 #Set index to titleWord before rating.
-                while titleWordIndex > -1  and (submissionTitleWords[titleWordIndex][0].isupper() or submissionTitleWords[titleWordIndex][1].isupper()): #If substirng is a pronoun, add to routeName.
-                    routeName = submissionTitleWords[titleWordIndex] + ' ' + routeName
-                    titleWordIndex = titleWordIndex - 1
+            # Create list of all words with all non-aplhabet chars removed.
+            originalSubmisonTitleWords = self.submission.title.split()
+            revisedSubmissionTitleWords = []
+            for word in originalSubmisonTitleWords:
+                revisedWord = re.sub('[^a-zA-Z]', '', word).strip()
+                if revisedWord != '':
+                    revisedSubmissionTitleWords.append(revisedWord)
 
-                #Remove extra characters from values if needed... parentheses, whitespace etc.
-                routeName = re.sub('([^\s\w]|_)+', '', routeName).strip()
-                rating = re.search('((5\.)?\d+[+-]?[a-dA-D]?)|([vV]\d+([/|\-]\d+)?)', submissionTitleWords[ratingIndex]).group(0)
+            # Determine rating index.
+            ratingIndex = None;
+            for index,titleWord in enumerate(originalSubmisonTitleWords):
+                if any (rating in titleWord for rating in ratings):
+                    ratingIndex = index
 
-                ##############
-                #Rating fixes#
-                ##############
-                #Example: V7/8 -> V7-8
-                if '\\' in rating or '/' in rating:
-                    rating = rating[0:2] + '-' + rating[3]
+            # Determine possible route names.
+            fillerWords = ['test']
+            possibleRouteNames = []
+            possibleRouteName = ''
+            for titleWord in revisedSubmissionTitleWords:
+                if (titleWord[0].isupper() and len(titleWord) > 1) or titleWord in fillerWords:
+                    possibleRouteName += titleWord + ' '
+                elif possibleRouteName != '' and possibleRouteName[0] != 'V':
+                    possibleRouteNames.append(possibleRouteName.strip())
+                    possibleRouteName = ''
 
-                #Example: v10 -> V10
-                if 'v' in rating:
-                    rating = rating.upper()
+            #Remove extra characters from values if needed... parentheses, whitespace etc.
+            for possibleRouteName in possibleRouteNames:
+                possibleRouteName = re.sub('([^\s\w]|_)+', '', possibleRouteName).strip()
+            rating = self.submission.title.split()[ratingIndex]
+            revisedRating = re.search('((5\.)?\d+[+-]?[a-dA-D]?)|([vV]\d+([/|\-]\d+)?)', originalSubmisonTitleWords[ratingIndex])
+            if revisedRating != None:
+                rating = revisedRating.group(0)
 
-                #Example: 11b -> 5.11b
-                if not 'V' in rating and not '5.' in rating:
-                    rating = '5.' + rating
+            ##############
+            #Rating fixes#
+            ##############
+            #Example: V7/8 -> V7-8
+            if '\\' in rating or '/' in rating:
+                rating = rating[0:2] + '-' + rating[3]
 
-                #Example: 5.10B -> 5.10b
-                if 'A' in rating or 'B' in rating or 'C' in rating or 'D' in rating:
-                    rating = rating.lower()
+            #Example: v10 -> V10
+            if 'v' in rating:
+                rating = rating.upper()
 
-                # submissionTitleUpperWords = []
-                # for titleWord in submissionTitleWords:
-                #     if titleWord[0].isupper():
-                #         submissionTitleUpperWords.append(titleWord)
-                #     else:
-                #         submissionTitleUpperWords.append('|')
-                #
-                # possibleRouteTitles = []
-                # possibleRouteTitle = ""
-                # for upperWord in submissionTitleUpperWords:
-                #     if upperWord != '|':
-                #         possibleRouteTitle = possibleRouteTitle + upperWord + ' '
-                #     else:
-                #         if possibleRouteTitle != "":
-                #             possibleRouteTitles.append(possibleRouteTitle)
-                #         possibleRouteTitle = ""
+            #Example: 11b -> 5.11b
+            if not 'V' in rating and not '5.' in rating:
+                rating = '5.' + rating
 
+            #Example: 5.10B -> 5.10b
+            if 'A' in rating or 'B' in rating or 'C' in rating or 'D' in rating:
+                rating = rating.lower()
 
-                time.sleep(10)
-                print('\n\n\nParsing new submission.')
-                print('Subreddit: ', self.submission.subreddit.display_name)
-                print('Submission title: ', self.submission.title)
-                seperator = "--------------------"
-                for char in self.submission.title:
-                    seperator = seperator + '-'
-                print(seperator)
-                #print('Possible Route Titles')
-                # for word in possibleRouteTitles:
-                #     print(word)
-                print('Detected route name: ', routeName)
-                print('Detected rating: ', rating)
+            print('\n\n\nParsing new submission.')
+            print('Subreddit: ', self.submission.subreddit.display_name)
+            print('Submission title: ', self.submission.title)
+            seperator = "--------------------"
+            for char in self.submission.title:
+                seperator = seperator + '-'
+            print(seperator)
+            print('Possible route names:')
+            for possibleRouteName in possibleRouteNames:
+                print('\t\"' + possibleRouteName + '\"')
+            print('Detected rating: ', rating)
 
-                return [routeName,rating]
+            return [possibleRouteNames,rating]
 
+        # No rating found.
+        return None
 
-    def getRouteWebAddress(self, titleInfoList):
-        #Determine route info from Reddit submission.
-        redditRouteName = titleInfoList[0]
-        redditRouteRating = titleInfoList[1]
-
+    def determineMountainProjectURL(self, possibleRouteName, redditRouteRating):
         #Determine Mountain Project search url for route.
         searchURL = 'https://www.mountainproject.com/search?q='
-        for word in redditRouteName.split(): searchURL = searchURL + word + '%20'
+        for word in possibleRouteName.split(): searchURL = searchURL + word + '%20'
         searchURL = searchURL[:-3] #Remove extra "%20".
         print('Determined search URL: ', searchURL)
 
@@ -144,7 +138,7 @@ class SubmissionHandler:
         #Each route displayed on the search page is now stored as a tuple in mtnProjectRoutes... (name, rating, link).
         #Determine a route and submission info match.
         for mtnProjectRoute in mtnProjectRoutes:
-            if redditRouteName == mtnProjectRoute[0]:
+            if possibleRouteName == mtnProjectRoute[0]:
                 if redditRouteRating == mtnProjectRoute[1]:
                     return 'https://www.mountainproject.com' + mtnProjectRoute[2]
 
@@ -160,15 +154,29 @@ class SubmissionHandler:
                     if redditRouteRating == mtnProjectRoute[1][0:5] or redditRouteRating == mtnProjectRoute[1][0:4] + mtnProjectRoute[1][6]:
                         return 'https://www.mountainproject.com' + mtnProjectRoute[2]
 
+
+    def getRouteWebAddress(self, titleInfoList):
+        #Determine route info from Reddit submission.
+        redditPossibleRouteNames = titleInfoList[0]
+        redditRouteRating = titleInfoList[1]
+
+        for possibleRouteName in redditPossibleRouteNames:
+            print(possibleRouteName)
+            possibleRouteURL = self.determineMountainProjectURL(possibleRouteName, redditRouteRating)
+            if possibleRouteURL is not None:
+                return [possibleRouteName, possibleRouteURL]
+
         #If no match found.
         print('No matching route found.')
         return None
 
 
     def getRouteInfo(self, titleInfoList):
-        routeWebAddress = self.getRouteWebAddress(titleInfoList)
-        if routeWebAddress is None:
+        routeNameAndWebAddress = self.getRouteWebAddress(titleInfoList)
+        if routeNameAndWebAddress is None:
             return None
+        routeName = routeNameAndWebAddress[0]
+        routeWebAddress = routeNameAndWebAddress[1]
         print('Determined matching route URL: ', routeWebAddress)
 
         #Use selenium webdriver to load HTML to BeautifulSoup.
@@ -185,9 +193,6 @@ class SubmissionHandler:
         #############################
         ######Gather route data######
         #############################
-        #Name
-        routeName = titleInfoList[0]
-
         #Location chain
         locationsLinks = routePageSoup.find('div', {'class': 'mb-half small text-warm'}).find_all('a')
         locationChain = ""
--- ClimbingRouteBot/rClimbingBot.py
+++ ClimbingRouteBot/rClimbingBot.py
@@ -5,80 +5,32 @@ import re
 import os
 import time
 import sys
+import traceback
 from datetime import datetime, timedelta
 
-#Clear screen.
+# Clear screen.
 os.system('cls' if os.name == 'nt' else 'clear')
 
-#Create list of replied post IDs from repliedPosts.txt.
+# Create list of replied post IDs from repliedPosts.txt.
 repliedPosts = []
 with open('repliedPosts.txt', 'r') as file:
     repliedPosts = file.read()
     repliedPosts = repliedPosts.split('\n')
     repliedPosts = list(filter(None, repliedPosts))
 
-# Create the Reddit instance and log in.
-reddit = praw.Reddit(client_id = 'LMxeepkXA8W5kg',
-                     client_secret = '834ts23jIxsRQO0_qH_Vq9eeGY0',
-                     username = 'ClimbingRouteBot',
-                     password = '$4Bluiloveyou',
-                     user_agent = 'Seth Peterson 2019')
-subreddit = reddit.subreddit('climbing+Bouldering+ClimbingPorn+ClimbingVids')
+# Read credentialsFile and create the Reddit instance.
+credentialsFile = open(os.path.dirname(__file__) + "\\..\\..\\ClimbingRouteBotInfo.txt", "r")
+credentials = credentialsFile.readlines()[1].split()
+reddit = praw.Reddit(client_id = credentials[0],
+                     client_secret = credentials[1],
+                     username = credentials[2],
+                     password = credentials[3],
+                     user_agent = credentials[4])
+subreddit = reddit.subreddit('climbing+Bouldering+socalclimbing+ClimbingPorn+ClimbingVids')
 
 while True:
     for submission in subreddit.new(limit=100):
-        # if not submission.is_self:
-        #
-        #     if submission.id not in repliedPosts:
-        #         submissionHandler = SubmissionHandler(submission)
-        #         titleInfoList = submissionHandler.getTitleInfo()
-        #
-        #         if titleInfoList is not None and titleInfoList[0] != '' and titleInfoList[1] != '':
-        #             #routeInfo = [routeName, locationChain, ydsRating, avgScore, routeType, firstAccent, description, mountainProjectLink]
-        #             routeInfo = submissionHandler.getRouteInfo(titleInfoList)
-        #
-        #             if routeInfo is not None:
-        #                 #Route header.
-        #                 comment = '## [' + routeInfo[0] + '](' + routeInfo[8] +' \"Mountain Project\")\n\n'
-        #                 comment += routeInfo[1] + '\n\n'
-        #
-        #                 #Table of info.
-        #                 comment += '***\n\n'
-        #                 comment += 'Rating: ' + routeInfo[2] + '\n\n'
-        #                 comment += 'Score ' + routeInfo[3] + '\n\n'
-        #                 comment += 'Type: ' + routeInfo[4] + '\n\n'
-        #                 comment += 'FA: ' + routeInfo[5] + '\n\n'
-        #
-        #                 #Description of the route.
-        #                 if routeInfo[6] is not None:
-        #                     comment += 'Description:\n\n'
-        #                     comment += '>' + routeInfo[6]
-        #
-        #                 #Protection of the route.
-        #                 if routeInfo[7] is not None:
-        #                     comment += '\n\nProtection:\n\n'
-        #                     comment += '>' + routeInfo[7]
-        #
-        #                 #Signature.
-        #                 comment += '\n\nI am a bot, beep boop.'
-        #
-        #                 submission.reply(comment)
-        #                 repliedPosts.append(submission.id)
-        #
-        #                 #Update repliedPosts.txt
-        #                 with open('repliedPosts.txt', 'w+') as file:
-        #                     for postID in repliedPosts:
-        #                         file.write(postID + '\n')
-        #
-        #                 print()
-        #                 timer = 400
-        #                 while timer is not 0:
-        #                     seconds = timedelta(seconds=timer)
-        #                     dateTime = datetime(1,1,1) + seconds
-        #                     sys.stdout.write('\rComment Posted. Continuing in: %s:%s' % (dateTime.minute, dateTime.second))
-        #                     sys.stdout.flush()
-        #                     time.sleep(1)
-        #                     timer = timer - 1
+
         try:
             if not submission.is_self:
 
@@ -86,7 +38,7 @@ while True:
                     submissionHandler = SubmissionHandler(submission)
                     titleInfoList = submissionHandler.getTitleInfo()
 
-                    if titleInfoList is not None and titleInfoList[0] != '' and titleInfoList[1] != '':
+                    if titleInfoList is not None:
                         #routeInfo = [routeName, locationChain, ydsRating, avgScore, routeType, firstAccent, description, mountainProjectLink]
                         routeInfo = submissionHandler.getRouteInfo(titleInfoList)
 
@@ -134,7 +86,8 @@ while True:
                                 time.sleep(1)
                                 timer = timer - 1
         except:
-            print('\nAn exception occured.\n')
+            traceback.print_exc()
+            time.sleep(1000)
 
 
 print()
